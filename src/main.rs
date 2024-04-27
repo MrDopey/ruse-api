ceb0c86bb71a041609d830a18ed9240ec0ca11c6
Gene Luong <beforewards@gmail.com>
2024-04-27 23:09:30 +1000

    added unit tests

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,30 @@ async fn hello() -> &'static str {
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    let router = Router::new().get(hello);
     let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
-    Server::new(acceptor).serve(router).await;
+    Server::new(acceptor).serve(route()).await;
+}
+
+fn route() -> Router {
+    Router::new().get(hello)
+}
+
+#[cfg(test)]
+mod tests {
+    use salvo::prelude::*;
+    use salvo::test::{ResponseExt, TestClient};
+
+    #[tokio::test]
+    async fn test_hello_word() {
+        let service = Service::new(super::route());
+
+        let content = TestClient::get(format!("http://127.0.0.1:5800/"))
+            .send(&service)
+            .await
+            .take_string()
+            .await
+            .unwrap();
+
+        assert_eq!(content, "Hello World");
+    }
 }
