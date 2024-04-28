5bf6bf6385fea2ad6e6dad63105854b773233edf
Gene Luong <beforewards@gmail.com>
2024-04-28 15:13:18 +1000

    feat(csp): added content security policy

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,69 @@
+use salvo::http::header::{self, HeaderValue};
 use salvo::prelude::*;
+use std::collections::HashMap;
 use std::env;
 
 #[handler]
 async fn hello() -> &'static str {
     "Hello World"
+}
+
+#[handler]
+// async fn add_content_security_policy(host: &str) -> impl Fn(&mut Response) -> () {
+async fn add_content_security_policy(res: &mut Response) -> () {
+    let maps = HashMap::from([
+        ("default-src", "'self' 'unsafe-inline' 'unsafe-eval'"),
+        ("style-src", "'self' 'unsafe-inline' 'unsafe-eval'"),
+        (
+            "script-src",
+            "https://appssdk.zoom.us/sdk.min.js 'self' 'unsafe-inline' 'unsafe-eval'",
+        ),
+        (
+            "img-src",
+            "'self' data: https://b7a9-124-168-11-219.ngrok-free.app",
+        ),
+        (
+            "connect-src",
+            "'self' wss://b7a9-124-168-11-219.ngrok-free.app",
+        ),
+        ("base-uri", "'self'"),
+        ("form-action", "'self'"),
+        ("font-src", "'self' https: data:"),
+        ("frame-ancestors", "'self'"),
+        ("object-src", "'none'"),
+        ("script-src-attr", "'none'"),
+        ("upgrade-insecure-requests", ""),
+    ]);
+
+    let value = maps.iter().fold("".to_owned(), |acc, it| {
+        format!("{}{} {};", acc, it.0, it.1)
+    });
+
+    let headers = res.headers_mut();
+
+    headers.insert(
+        header::CONTENT_SECURITY_POLICY,
+        HeaderValue::from_str(&value).unwrap(),
+    );
+    headers.insert(
+        header::STRICT_TRANSPORT_SECURITY,
+        HeaderValue::from_static("max-age=31536000; includeSubDomains"), // 1 year in seconds
+    );
+    headers.insert(
+        header::REFERRER_POLICY,
+        HeaderValue::from_static("same-origin"),
+    );
+    headers.insert(
+        header::X_FRAME_OPTIONS,
+        HeaderValue::from_static("SAMEORIGIN"),
+    );
+
+    // Doesn't work well
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+    // headers.insert(
+    //     header::X_XSS_PROTECTION,
+    //     HeaderValue::from_static("0"),
+    // );
 }
 
 #[tokio::main]
@@ -20,7 +80,7 @@ async fn main() {
 }
 
 fn route() -> Router {
-    Router::new().get(hello)
+    Router::new().hoop(add_content_security_policy).get(hello)
 }
 
 #[cfg(test)]
